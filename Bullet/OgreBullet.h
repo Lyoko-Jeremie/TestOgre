00b3caf2f79fdbc2ff2a52aeaa01dc4543b6ec95
LyokoJeremie <lucheng989898@gmail.com>
2023-04-24 22:42:39 +0800

    Bullet2OgreTracer

--- Bullet/OgreBullet.h
+++ Bullet/OgreBullet.h
@@ -8,8 +8,11 @@
 
 #include <utility>
 
+#include <boost/assert.hpp>
+
 #include "btBulletDynamicsCommon.h"
-#include "Ogre.h"
+#include <Ogre.h>
+#include <OgreApplicationContext.h>
 
 #include "./BulletMemoryContainer.h"
 
@@ -47,6 +50,7 @@ namespace Ogre {
                 ret = btTransform(convert(mNode->getOrientation()), convert(mNode->getPosition()));
             }
 
+            // Bullet will call this function to update Ogre object info
             void setWorldTransform(const btTransform &in) override {
                 btQuaternion rot = in.getRotation();
                 btVector3 pos = in.getOrigin();
@@ -224,6 +228,116 @@ namespace Ogre {
         private:
 
             void onTick();
+
+        public:
+
+            struct Bullet2OgreTracer : public boost::enable_shared_from_this<Bullet2OgreTracer> {
+
+                // ------ memory host information ------
+                boost::weak_ptr<OgreBites::ApplicationContext> pCtx;
+                Ogre::Root *root;
+                Ogre::SceneManager *scnMgr;
+                std::string nameSceneManager;
+                // ------ memory host information ------
+
+                Ogre::SceneNode *sceneNode;
+                std::string sceneNodeName;
+                Entity *entity;
+
+                Bullet2OgreTracer(
+                        const boost::shared_ptr<OgreBites::ApplicationContext> &ctx,
+                        Ogre::Root *root,
+                        Ogre::SceneManager *scnMgr,
+                        Entity *ent
+                ) : pCtx(ctx), root(root), scnMgr(scnMgr), entity(ent) {
+                    BOOST_ASSERT_MSG(
+                            !pCtx.expired() && root && scnMgr && sceneNode && entity,
+                            "Bullet2OgreTracer invalid Ogre info"
+                    );
+                    sceneNode = ent->getParentSceneNode();
+                    sceneNodeName = sceneNode->getName();
+                }
+
+
+                /** check the scene still valid and the node still in the scene
+                 * this is a slow operator, call it carefully
+                 */
+                bool checkValid() const {
+
+                    // check context top-down
+                    if (pCtx.expired() || !root || !scnMgr || !sceneNode || !entity) {
+                        return false;
+                    }
+                    auto ctx = pCtx.lock();
+                    if (!ctx) {
+                        return false;
+                    }
+                    if (root != ctx->getRoot()) {
+                        return false;
+                    }
+                    auto scnMgrPtr = root->getSceneManager(nameSceneManager);
+                    if (scnMgrPtr != scnMgr) {
+                        return false;
+                    }
+
+                    const String &nameToFind = sceneNodeName;
+                    SceneNode *nodeToFind = sceneNode;
+                    SceneNode *nodeRootToFindStart = scnMgrPtr->getRootSceneNode();
+
+                    // https://forums.ogre3d.org/viewtopic.php?t=20799
+
+                    BOOST_ASSERT_MSG(nodeRootToFindStart, "nodeRootToFindStart must not nullptr");
+                    BOOST_ASSERT_MSG(
+                            nodeToFind || !nameToFind.empty(),
+                            "nodeToFind||nameToFind must valid at lest one"
+                    );
+
+                    // if node have name, direct find use name
+                    auto p = scnMgrPtr->getSceneNode(nameToFind, false);
+                    if (!nameToFind.empty()) {
+                        if (p) {
+                            return true;
+                        }
+                    }
+
+                    BOOST_ASSERT_MSG(nodeToFind, "nodeToFind must not nullptr when nameToFind.empty()");
+
+                    // a deep first search use stack(std::ref)
+                    //    std::deque(std::pair( ChildNodeMap& , ChildNodeMap_Index ))
+                    std::deque<std::pair<std::reference_wrapper<const SceneNode::ChildNodeMap>, int>> stack;
+
+                    stack.emplace_back(nodeRootToFindStart->getChildren(), 0);
+
+                    while (!stack.empty()) {
+                        auto N = stack.rbegin();
+                        if (N == stack.rend()) {
+                            // empty, still not find
+                            return false;
+                        }
+                        if (N->second >= N->first.get().size()) {
+                            // this node look end, now to look next node
+                            stack.pop_back();
+                            continue;
+                        }
+                        Node *n = N->first.get().at(N->second);
+                        if (n == nodeToFind) {
+                            // get it
+                            return true;
+                        }
+                        // next
+                        ++N->second;
+                        if (!n) {
+                            // invalid ptr
+                            continue;
+                        }
+                        // save child list, now try to look the list
+                        stack.emplace_back(n->getChildren(), 0);
+                    }
+
+                    return false;
+                }
+
+            };
 
         };
 
